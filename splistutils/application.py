71141c323a9da7fa57306c1138b0423819199f92
Corey S. Seliger <corey@seligerfamily.org>
2020-06-04 22:31:02 -0400

    WIP: Basic read worksheet

--- splistutils/application.py
+++ splistutils/application.py
@@ -1,6 +1,13 @@
 
 import logging
 
+import pandas as pd
+
+from openpyxl import load_workbook
+from pprint import pprint
+from itertools import islice
+
+
 log = logging.getLogger(__name__)
 
 class SharePointListUtils:
@@ -9,3 +16,19 @@ class SharePointListUtils:
     @staticmethod
     def run():
         log.info("SharePoint List Utilities - Starting up...")
+
+        wb = load_workbook(filename='data.xlsx', read_only=True)
+        ws = wb['Raw Data']
+
+        data = ws.values
+        columns = next(data)[1:]
+        data = list(data)
+        idx = [r[0] for r in data]
+        data = (islice(r, 1, None) for r in data)
+        df = pd.DataFrame(data, index=idx, columns=columns)
+
+
+        print(df.loc[[1130359]])
+
+
+     
